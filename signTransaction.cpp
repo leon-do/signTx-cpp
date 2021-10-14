bf359600c230dff51a19838c788902d0be448d28
Leon Do <leondo123@gmail.com>
2021-10-14 10:17:41 -0400

    Update signTransaction.cpp

--- signTransaction.cpp
+++ signTransaction.cpp
@@ -3,15 +3,15 @@
 using namespace std;
 
 static string signTransaction(string privateKey, string unsignedTxHash) {
-    // use private key 78dae1a22c7507a4ed30c06172e7614eb168d3546c13856340771e63ad3c0081
-    // to sign the unsigned transaction hash 0xfdb68ad9e5fbf87e7de81fa679204c5b95e5e25a37a591f001ef7edf486ddb96 
-    // to get signed transaction hash 0x4fe283b43618c83aaf9f4b1c488624f652e27d83bf32b74b908edc540703349e5337352805dc9609a5557924d5a10e4ef9c2e3315623f178fe85a49f4e475bf31b
-    return "0x4fe283b43618c83aaf9f4b1c488624f652e27d83bf32b74b908edc540703349e5337352805dc9609a5557924d5a10e4ef9c2e3315623f178fe85a49f4e475bf31b";
+    // use private key 0x78dae1a22c7507a4ed30c06172e7614eb168d3546c13856340771e63ad3c0081
+    // to sign the unsigned transaction hash 0xf94cc9053176e5b409587ebd640b31787b1db1e6db9b12ec8bd28760ab9dd9da 
+    // to get signed transaction hash 0x4b0927e304ea62a34bde5a33d882a101ca1b595f7686aaa421cbe75dc65344d26104d10cf81ec2b4ad3185591c36d782e10b73d48bc39a7ebadee9a0075044b81c
+    return "0x4b0927e304ea62a34bde5a33d882a101ca1b595f7686aaa421cbe75dc65344d26104d10cf81ec2b4ad3185591c36d782e10b73d48bc39a7ebadee9a0075044b81c";
 }
 
 int main() {
     string privateKey = "78dae1a22c7507a4ed30c06172e7614eb168d3546c13856340771e63ad3c0081";
-    string unsignedTxHash = "0xfdb68ad9e5fbf87e7de81fa679204c5b95e5e25a37a591f001ef7edf486ddb96";
+    string unsignedTxHash = "0xf94cc9053176e5b409587ebd640b31787b1db1e6db9b12ec8bd28760ab9dd9da";
     string signedTransaction = signTransaction(privateKey, unsignedTxHash);
     cout << signedTransaction << endl;
     return 0;
